3c1a512ab26df4828b0165774c7e9c4773f0d75d
Jintae Lee <37683734+TAEKnical@users.noreply.github.com>
2019-03-14 17:07:16 +0900

    Add files via upload

---
+++ U-04.sh
@@ -0,0 +1,34 @@
+#! /bin/bash
+
+
+source myfunctions.sh
+
+#[U-04]
+echo "[U-04] 패스워드 파일 보호"
+#step1
+if [ $(find "/etc/shadow") ];then
+   pass
+   echo "/etc/shadow 존재"
+else
+   warning
+   echo "/etc/shadow 부재"
+fi
+
+#step2
+error=0
+while read line
+do
+   if [[ !("$line" =~ ":x:") ]];then
+      warning
+      echo "두 번째 필드가 x로 설정되지 않은 계정이 있습니다."
+      error=1
+      break
+   fi
+done < /etc/passwd
+
+if [ $error -eq 0 ];then
+   pass
+   echo "패스워드 파일 보호"
+fi
+
+echo -e -n "\n"
